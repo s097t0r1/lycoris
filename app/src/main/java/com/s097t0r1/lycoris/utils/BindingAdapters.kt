d986e40aaeca10a0316b7d2c6a5e8b737a45b43c
s097t0r1 <zmitr241@yandex.ru>
2020-11-19 18:16:55 +0300

    feat(*): add placeholders for images

--- app/src/main/java/com/s097t0r1/lycoris/utils/BindingAdapters.kt
+++ app/src/main/java/com/s097t0r1/lycoris/utils/BindingAdapters.kt
@@ -3,12 +3,15 @@ package com.s097t0r1.lycoris.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.s097t0r1.lycoris.R
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(url: String?) {
     url?.let {
         Glide.with(this.context)
             .load(it)
+            .placeholder(R.drawable.ic_baseline_photo_24)
+            .error(R.drawable.ic_baseline_error_outline_24)
             .centerCrop()
             .into(this)
     }
