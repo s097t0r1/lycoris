6b3e889b9fc37bc00626aa055bea8b6066c1a478
s097t0r1 <zmitr241@yandex.ru>
2020-11-19 14:16:37 +0300

    fix(di): add missing annotations
    Add missing @Singleton and @Provides annotations for DatabaseModule

--- app/src/main/java/com/s097t0r1/lycoris/di/DatabaseModule.kt
+++ app/src/main/java/com/s097t0r1/lycoris/di/DatabaseModule.kt
@@ -4,14 +4,18 @@ import android.content.Context
 import com.s097t0r1.lycoris.data.source.local.DAO
 import com.s097t0r1.lycoris.data.source.local.PhotoDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @InstallIn(ApplicationComponent::class)
 @Module
 object DatabaseModule {
 
+    @Singleton
+    @Provides
     fun provideDatabaseDAO(@ApplicationContext context: Context): DAO {
         return PhotoDatabase.getInstance(context).databaseDAO()
     }
