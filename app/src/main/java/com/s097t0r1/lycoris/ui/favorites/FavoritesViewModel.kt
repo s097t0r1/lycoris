9dd570a19d4329c9ec515b7c3f1308a48af34a72
s097t0r1 <zmitr241@yandex.ru>
2020-11-21 16:06:27 +0300

    fix(ui.favorites): fix bug with empty result
    When an empty result was received, a notification about it was not displayed

--- app/src/main/java/com/s097t0r1/lycoris/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/s097t0r1/lycoris/ui/favorites/FavoritesViewModel.kt
@@ -5,10 +5,9 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.s097t0r1.lycoris.data.Photo
 import com.s097t0r1.lycoris.data.Success
+import com.s097t0r1.lycoris.data.Error
 import com.s097t0r1.lycoris.data.source.PhotoRepository
 import kotlinx.coroutines.launch
-import java.lang.Error
-import java.lang.RuntimeException
 
 class FavoritesViewModel @ViewModelInject constructor(
     @Assisted private val savedStateHandle: SavedStateHandle,
@@ -27,28 +26,24 @@ class FavoritesViewModel @ViewModelInject constructor(
     val errorLoadingData: LiveData<Boolean>
         get() = _errorLoadingData
 
-    private val _loadingData = MutableLiveData<Boolean>(false)
-    val loadingData: LiveData<Boolean>
-        get() = _loadingData
+    private val _dataLoading = MutableLiveData<Boolean>(false)
+    val dataLoading: LiveData<Boolean>
+        get() = _dataLoading
 
     init {
         getPhotos()
     }
 
     fun getPhotos() {
-        _errorLoadingData.value = false
-        _emptyResult.value = false
-
         viewModelScope.launch {
-            _loadingData.value = true
+            _emptyResult.value = false
+            _dataLoading.value = true
             val result = photoRepository.getPhotos(false)
-
             when(result) {
-                is Success -> if(result.data.isEmpty()) _emptyResult.value = true else _photos.value = result.data
-                is Error -> _errorLoadingData.value = true
+                is Success -> _photos.value = result.data
+                is Error -> _emptyResult.value = true
             }
-
-            _loadingData.value = false
+            _dataLoading.value = false
         }
     }
 
